71a510b304b24b56822493dd0ee4f631bc089878
CruelEngine <babhineethbhat@gmail.com>
2018-09-15 21:27:52 +0530

    search component added

--- dashboard/src/app/dashboard-interface/dashboard-interface.module.ts
+++ dashboard/src/app/dashboard-interface/dashboard-interface.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardInterfaceRoutingModule } from './dashboard-interface-routing.module';
 import { InterfaceComponent } from './interface/interface.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardInterfaceRoutingModule
   ],
-  declarations: [InterfaceComponent]
+  declarations: [InterfaceComponent, SearchComponent]
 })
 export class DashboardInterfaceModule { }
