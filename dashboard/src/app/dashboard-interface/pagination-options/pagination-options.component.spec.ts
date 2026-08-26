dd597cb9b7bfd2485270baa13123194e10ddbe6e
CruelEngine <babhineethbhat@gmail.com>
2026-08-26 18:59:56 +0530

    feat: update to ng 11

--- dashboard/src/app/dashboard-interface/pagination-options/pagination-options.component.spec.ts
+++ dashboard/src/app/dashboard-interface/pagination-options/pagination-options.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PaginationOptionsComponent } from './pagination-options.component';
 
@@ -6,7 +6,7 @@ describe('PaginationOptionsComponent', () => {
   let component: PaginationOptionsComponent;
   let fixture: ComponentFixture<PaginationOptionsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PaginationOptionsComponent ]
     })
